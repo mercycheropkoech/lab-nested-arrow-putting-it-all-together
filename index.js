fee77361c55458a4a7c61c81a31e7a45da65f2f9
MERCYCHEROP <mercy.cherop@student.moringaschool.com>
2026-04-14 14:45:45 +0300

    tested

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+ return (passwordAttempt) => {
+    attemptCount++;
 
+    // If already exceeded attempts
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
 
+    // Check password
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
